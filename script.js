470bf1fd230c71f8873dfed6918cf65992b4b700
rishyendra333 <129914241+rishyendra333@users.noreply.github.com>
2023-08-30 11:28:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("newBlog");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+    // Navigate to the addBlog.html page
+    window.location.href = "addBlog.html";
+});
