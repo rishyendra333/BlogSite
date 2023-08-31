37be6ca2d332a3e44f220ae904ceca94edc45c41
rmedamanuri <97180576+rmedamanuri@users.noreply.github.com>
2023-08-31 13:19:49 -0400

    Co-authored-by: Pranav Narala <PranavNarala1@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+// import React, { useEffect } from 'react';
+// import './App.css'; // You can include your global styles here
+
+// function Header() {
+//   return (
+//     <header>
+//       <h1>Welcome to the Blog</h1>
+//     </header>
+//   );
+// }
+
+// function NewBlogButton() {
+//   useEffect(() => {
+//     const script = document.createElement('script');
+//     script.src = '../script.js';
+//     script.async = true;
+//     document.body.appendChild(script);
+
+//     return () => {
+//       document.body.removeChild(script);
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <p>If you want to make a new post, click the button below!</p>
+//       <button id="newBlog">New Blog!</button>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <NewBlogButton />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useEffect } from 'react';
 import './App.css';
+import {
+  BrowserRouter,
+  Link,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+//pages
+import Home from "./components/Home";
+import AddBlog from "./components/AddBlog";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* This is the alias of BrowserRouter i.e. Router */}
+      <BrowserRouter>
+        <Routes>
+          {/* This route is for home component 
+          with exact path "/", in component props 
+          we passes the imported component*/}
+          <Route exact path="/" component={Home} />
+            
+          {/* This route is for about component 
+          with exact path "/about", in component 
+          props we passes the imported component*/}
+          <Route path="/AddBlog" component={AddBlog} />
+            
+          {/* If any route mismatches the upper 
+          route endpoints then, redirect triggers 
+          and redirects app to home component with to="/" */}
+          <Navigate to="/" />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
-
+  
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+// importing Link from react-router-dom to navigate to 
+// different end points.
+import { Link } from "react-router-dom";
+  
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <br />
+      <ul>
+        <li>
+          {/* Endpoint to route to Home component */}
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          {/* Endpoint to route to About component */}
+          <Link to="/addBlog">Add Blog</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+  
+export default Home;
