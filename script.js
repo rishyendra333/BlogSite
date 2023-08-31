e8166a38825eb12f2bff16350c265c1cce0016f4
rishyendra333 <129914241+rishyendra333@users.noreply.github.com>
2023-08-30 21:08:03 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var button = document.getElementById("newBlog");
-
-// Add a click event listener to the button
-button.addEventListener("click", function() {
-    // Navigate to the addBlog.html page
-    window.location.href = "addBlog.html";
-});
