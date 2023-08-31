89d54489327c6aac9c959d2dc7eb9075553a2e57
rishyendra333 <129914241+rishyendra333@users.noreply.github.com>
2023-08-30 21:09:18 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,82 @@
+// import React, { useEffect } from 'react';
+// import './App.css'; // You can include your global styles here
+
+// function Header() {
+//   return (
+//     <header>
+//       <h1>Welcome to the Blog</h1>
+//     </header>
+//   );
+// }
+
+// function NewBlogButton() {
+//   useEffect(() => {
+//     const script = document.createElement('script');
+//     script.src = '../script.js';
+//     script.async = true;
+//     document.body.appendChild(script);
+
+//     return () => {
+//       document.body.removeChild(script);
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <p>If you want to make a new post, click the button below!</p>
+//       <button id="newBlog">New Blog!</button>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <NewBlogButton />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useEffect } from 'react';
+import './App.css'; // You can include your global styles here`
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Home from "./components/Home";
+
+import AddBlog from "./components/AddBlog";
+
+function App() {
+  return (
+    <>
+      {/* This is the alias of BrowserRouter i.e. Router */}
+      <Router>
+        <Routes>
+          {/* This route is for home component 
+          with exact path "/", in component props 
+          we passes the imported component*/}
+          <Route exact path="/" component={Home} />
+            
+          {/* This route is for about component 
+          with exact path "/about", in component 
+          props we passes the imported component*/}
+          <Route path="/addBlog" component={AddBlog} />
+            
+          {/* If any route mismatches the upper 
+          route endpoints then, redirect triggers 
+          and redirects app to home component with to="/" */}
+          <Navigate to="/" />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+  
+export default App;
